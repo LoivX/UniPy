b3d32536935e669030ba201bd661c94d0836a822
Simone <simone.viol472@gmail.com>
2023-10-20 07:56:10 +0000

    deleted main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import numpy 
-
-# Function to print the 2D array
-def print2D(arr):
-    for row in arr:
-        print(row)
-
-array = numpy.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-print2D(array)
